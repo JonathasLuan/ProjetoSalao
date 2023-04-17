92958dd33ae13dc4eac3f98f22bfeb892d853cd1
Jonathas Luan <jonathas.araujo3@etec.sp.gov.br>
2023-04-17 05:54:28 -0300

    Criação de sistema de cadastro simples em PHP

---
+++ PHP/PHP2/index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Cadastro:</h1>
+    <?php
+    if(isset($_SESSION['msg']));
+    echo $_SESSION['msg'];
+    unset($_SESSION['msg']);
+    ?>
+    <form method="POST" action="processa.php">
+        <label for="">Nome:</label>
+        <input type="text" name="nome">
+        <br><br>
+        <label for="">E-mail:</label>
+        <input type="email" name="email">
+        <br><br>
+        <label for="">Senha:</label>
+        <input type="password" name="senha">
+        <br><br>
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+
+</html>
