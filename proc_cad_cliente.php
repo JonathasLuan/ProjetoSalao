25e437abb8a6bcec3517cc9cf556da8101a23a53
jonathas luan <jonathas.araujo3@etec.sp.gov.br>
2023-05-15 22:17:59 -0300

    adição de last id insert

--- proc_cad_cliente.php
+++ proc_cad_cliente.php
@@ -2,7 +2,7 @@
 session_start();
 include_once("conexao.php");
 
-// Manda para CONTINUAÇÃO DE CADASTRO 1 (contcad1)
+// Manda para CONTINUAÇÃO DE CADASTRO 1 (cad_end-cliente)
 
 $tipo = filter_input(INPUT_POST, 'tipo', FILTER_SANITIZE_STRING);
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
@@ -15,17 +15,16 @@ $genero = filter_input(INPUT_POST, 'genero', FILTER_SANITIZE_STRING);
 $result_usuario = "INSERT INTO usuário (tipo, nome, sobrenome, email, senha, telefone, genero) VALUES ('$tipo', '$nome', '$sobrenome', '$email', '$senha', '$telefone', '$genero')";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
-$sql = "SELECT id_usuario FROM usuário WHERE email = '$email'";
+/*$sql = "SELECT id_usuario FROM usuário WHERE email = '$email'";
 $resultado = mysqli_query($conn, $sql);
 $dados = mysqli_fetch_array($resultado);
-$_SESSION['id_usuario'] = $dados['id_usuario'];
+$_SESSION['id_usuario'] = $dados['id_usuario'];*/
 
+$_SESSION['id_usuario'] = "SELECT LAST_INSERT_ID()";
 
 if (mysqli_insert_id($conn)) {
-    $_SESSION['msg'] = "Usuário cadastrado com sucesso";
     header("Location: cad_end-cliente.php");
 } else {
     header("Location: cadastro.php");
     echo "Falha ao cadastrar.";
 }
-?>
--- proc_cad_prof.php
+++ proc_cad_prof.php
@@ -2,6 +2,8 @@
 session_start();
 include_once("conexao.php");
 
+// Manda para CONTINUAÇÃO DE CADASTRO 1 (cad_end-prof)
+
 $tipo = filter_input(INPUT_POST, 'tipo', FILTER_SANITIZE_STRING);
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
 $sobrenome = filter_input(INPUT_POST, 'sobrenome', FILTER_SANITIZE_STRING);
@@ -13,11 +15,16 @@ $genero = filter_input(INPUT_POST, 'genero', FILTER_SANITIZE_STRING);
 $result_usuario = "INSERT INTO usuário (tipo, nome, sobrenome, email, senha, telefone, genero) VALUES ('$tipo', '$nome', '$sobrenome', '$email', '$senha', '$telefone', '$genero')";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
+/*$sql = "SELECT id_usuario FROM usuário WHERE email = '$email'";
+$resultado = mysqli_query($conn, $sql);
+$dados = mysqli_fetch_array($resultado);
+$_SESSION['id_usuario'] = $dados['id_usuario'];*/
+
+$_SESSION['id_usuario'] = "SELECT LAST_INSERT_ID()";
+
 if (mysqli_insert_id($conn)) {
-    $_SESSION['msg'] = "Usuário cadastrado com sucesso";
     header("Location: cad_end-prof.php");
-}else {
+} else {
     header("Location: cadastro.php");
     echo "Falha ao cadastrar.";
 }
-?>
