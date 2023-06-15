2f626f4c91a0ec7644a2a703adad34900aa8b0dd
Jonathas Luan <jonathas.araujo3@etec.sp.gov.br>
2023-06-14 22:18:39 -0300

    homecss para home

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['id']) && session_id() == $_SESSION['id']) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>SalonSet</title>
   <link rel="stylesheet" href="./index.css">
-  <link rel="stylesheet" href="./homeCSS.css">
+  <link rel="stylesheet" href="./home.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
