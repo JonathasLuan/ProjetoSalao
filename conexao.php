85c3f3c39948b45d0a5940d58f1f6ebbf6e14a0d
jonathas luan <jonathas.araujo3@etec.sp.gov.br>
2024-03-09 19:19:35 -0300

    ajusta conexao

--- conexao.php
+++ conexao.php
@@ -1,4 +1,19 @@
 <?php
+// Configurações do banco de dados
+$host = '127.0.0.1';        // Endereço do servidor MySQL
+$username = 'root';  // Nome de usuário do MySQL
+$password = '131544741Jl@';    // Senha do MySQL
+$database = 'projetosalao';    // Nome do banco de dados
+
+// Estabelece a conexão com o banco de dados
+$conn = new mysqli($host, $username, $password, $database);
+
+// Verifica se houve erros na conexão
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+/*
 $servidor = "localhost";
 $usuario = "projetosalao";
 $senha = "projetosalao";
@@ -20,4 +35,6 @@ $mysqli = new mysqli($host, $usuario, $senha, $database);
 if($mysqli->error) {
     die("falha ao conectar ao banco de dados: " . $mysqli->error);
 }
+*/
+
 ?>
